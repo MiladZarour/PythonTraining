65a1ec42beab20d2b020e0256078f10e5df6fe0b
Milad Zarour <50852900+MiladZarour@users.noreply.github.com>
2021-11-15 16:22:21 +0100

    again showing the difference between set and list

--- 6-BasicSetOperations&Syntax/set.py
+++ 6-BasicSetOperations&Syntax/set.py
@@ -4,3 +4,10 @@ for element in my_set:
 
 my_set.add("April")  # added an element to the set
 print(my_set)
+my_set.remove("January")
+print(my_set)
+
+
+my_list = ["January", "February", "March", "January"]
+my_list.remove("January")
+print(my_list)
