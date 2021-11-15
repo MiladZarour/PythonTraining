f0382a7aa739ef0c5d4aaafa9437691dc68d369e
Milad Zarour <50852900+MiladZarour@users.noreply.github.com>
2021-11-15 17:45:47 +0100

    Built in function examples

--- 10-BuiltInModulesFunctions/main.py
+++ 10-BuiltInModulesFunctions/main.py
@@ -1,9 +1,17 @@
 from helper import validate_and_excute, user_input_message
+import logging, os
 
-user_input = ""
-while user_input != "exit":
-    user_input = input(user_input_message)
-    days_and_unit = user_input.split(":")
-    print(days_and_unit)
-    days_and_unit_dictionary = {"days": days_and_unit[0],"unit": days_and_unit[1]}
-    validate_and_excute()
+my_os = os.name
+print("my_os : ", my_os)
+logger = logging.getLogger("MAIN")
+logger.error("Error happened in the app")
+
+
+
+# user_input = ""
+# while user_input != "exit":
+#     user_input = input(user_input_message)
+#     days_and_unit = user_input.split(":")
+#     print(days_and_unit)
+#     days_and_unit_dictionary = {"days": days_and_unit[0],"unit": days_and_unit[1]}
+#     validate_and_excute()
