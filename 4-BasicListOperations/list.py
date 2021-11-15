9a3271afa6f8a01f744bb6377c4287ee3c92d7ba
Milad Zarour <50852900+MiladZarour@users.noreply.github.com>
2021-11-15 16:00:49 +0100

    fail index error

--- 4-BasicListOperations/list.py
+++ 4-BasicListOperations/list.py
@@ -1,4 +1,6 @@
 my_list = ["January", "February", "March"]
 print(my_list[2])  # getting specific value
 my_list.append("April")  # added element to the list
-print(my_list)
+print(my_list[])
+print(my_list[3])
+# print(my_list[4]) #fail index error
