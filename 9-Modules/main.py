fa0f7c8b98dcd3f7f30bc7816a0655cab92a1794
Milad Zarour <50852900+MiladZarour@users.noreply.github.com>
2021-11-15 17:34:10 +0100

    import helper as h

--- 9-Modules/main.py
+++ 9-Modules/main.py
@@ -1,10 +1,9 @@
-from helper import validate_and_excute, user_input_message
-from helper import * # it's a waste if just we need to import a little
+import helper as h
 
 user_input = ""
 while user_input != "exit":
-    user_input = input(user_input_message)
+    user_input = input(h.user_input_message)
     days_and_unit = user_input.split(":")
     print(days_and_unit)
     days_and_unit_dictionary = {"days": days_and_unit[0],"unit": days_and_unit[1]}
-    validate_and_excute()
+    h.validate_and_excute()
