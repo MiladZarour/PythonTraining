a8da3799997401a2dd1d897c627095197ce66971
Milad Zarour <50852900+MiladZarour@users.noreply.github.com>
2021-11-16 07:38:31 +0100

    # calculation total of inventory per supplier value per supplier

--- 13-AutomationWithPython/main.py
+++ 13-AutomationWithPython/main.py
@@ -9,10 +9,12 @@ for product_row in range(2, product_list.max_row + 1):
     #print(product_list.cell(product_row, 4))
     supplier_name = product_list.cell(product_row, 4).value
 
+    # calculation number of products per supplier
     if supplier_name in products_per_supplier:
         current_num_products = products_per_supplier.get(supplier_name)
         products_per_supplier[supplier_name] = current_num_products + 1
     else:
         products_per_supplier[supplier_name] = 1
 
-print(products_per_supplier)
+
+# calculation total of inventory per supplier value per supplier
