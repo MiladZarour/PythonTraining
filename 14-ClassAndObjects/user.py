a68598ddee501e4a8a66f0ad753856b3d5e366d6
Milad Zarour <50852900+MiladZarour@users.noreply.github.com>
2021-11-16 08:17:53 +0100

    class User:

---
+++ 14-ClassAndObjects/user.py
@@ -0,0 +1,19 @@
+class User:
+
+
+email = "nn@nn.com"
+name = "Nana Janashia"
+password = "pwd"
+current_job_title = "DevOps Engineer"
+
+
+def change_password():
+    # do smth
+
+
+def current_job_title():
+    # do smth
+
+
+def add_new_skill():
+    # do smth
