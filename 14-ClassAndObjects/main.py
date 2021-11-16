70d5aff13482b74415bd0dffc5e3e7ed8260585c
Milad Zarour <50852900+MiladZarour@users.noreply.github.com>
2021-11-16 08:43:26 +0100

    by using user.User , we can go inside that class

--- 14-ClassAndObjects/main.py
+++ 14-ClassAndObjects/main.py
@@ -1,8 +1,9 @@
 import user  # we import the file name (be careful)
 
-app_user_one = User("nn@nn.com", "Nana Janashia", "pwd1", "DecOps Engineer")
+app_user_one = user.User("nn@nn.com", "Nana Janashia",
+                         "pwd1", "DecOps Engineer")
 app_user_one.get_user_info()
 
 
-app_user_two = User("aa@aa.com", "James Bond", "SuperSecret", "Agent")
+app_user_two = user.User("aa@aa.com", "James Bond", "SuperSecret", "Agent")
 app_user_two.get_user_info()
