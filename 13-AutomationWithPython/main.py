973c0daf6509e6c8530658d97e9fdb52279197ab
Milad Zarour <50852900+MiladZarour@users.noreply.github.com>
2021-11-16 07:16:06 +0100

    opening file xlsx

--- 13-AutomationWithPython/main.py
+++ 13-AutomationWithPython/main.py
@@ -2,3 +2,10 @@ import openpyxl
 
 inv_file = openpyxl.load_workbook("inventory.xlsx")
 product_list = inv_file["Sheet1"]
+
+products_per_supplier = {}
+
+print(product_list.max_row)
+
+for product_row in range(2, product_list.max_row):
+    print()
---
+++ 13-AutomationWithPython/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+product_list = inv_file["Sheet1"]
+
+# products_per_supplier = {}
+
+# print(product_list.max_row)
