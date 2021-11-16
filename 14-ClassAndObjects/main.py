ffd6bd8f60dc09445e944037cbf34f15c5e9eb5e
Milad Zarour <50852900+MiladZarour@users.noreply.github.com>
2021-11-16 08:44:23 +0100

    from user import User

--- 14-ClassAndObjects/main.py
+++ 14-ClassAndObjects/main.py
@@ -1,9 +1,9 @@
-import user  # we import the file name (be careful)
+from user import User  # we import from the file user , the class User
 
-app_user_one = user.User("nn@nn.com", "Nana Janashia",
-                         "pwd1", "DecOps Engineer")
+app_user_one = User("nn@nn.com", "Nana Janashia",
+                    "pwd1", "DecOps Engineer")
 app_user_one.get_user_info()
 
 
-app_user_two = user.User("aa@aa.com", "James Bond", "SuperSecret", "Agent")
+app_user_two = User("aa@aa.com", "James Bond", "SuperSecret", "Agent")
 app_user_two.get_user_info()
